6896bbf04049cdd8a53e867cdffb00edb2fe4d26
wfd1606 <529275872@qq.com>
2019-01-16 01:24:30 +0800

    icon

--- src/page/layout/NavBarItem.vue
+++ src/page/layout/NavBarItem.vue
@@ -1,12 +1,24 @@
 <template>
     <el-submenu v-if="item.child && item.child.length && item.isChildMenu == 1" :index="navIndex"> 
         <!-- 创建父级菜单 -->
-        <template slot="title"><i v-if="item.icon" class="item.icon"></i>{{ item.title }}</template>
+        <template slot="title">
+            <span v-if="item.icon" class="svg-container">
+                <svg-icon :icon-class=item.icon />
+            </span>
+            <span>{{ item.title }}</span>
+            <!-- <i v-if="item.icon" class="item.icon"></i>{{ item.title }} -->
+        </template>
         <!-- 创建子菜单 -->
         <nav-bar-item v-for="(subItem,i) in item.child" :key="navIndex+'-'+i" :navIndex="navIndex+'-'+i" :item="subItem" ></nav-bar-item>
     </el-submenu>
 
-    <el-menu-item v-else :index="item.name" :route="{path: item.name}"><i v-if="item.icon" class="item.icon"></i>{{ item.title }}</el-menu-item>
+    <el-menu-item v-else :index="item.name" :route="{path: item.name}">
+        <span v-if="item.icon" class="svg-container">
+          <svg-icon :icon-class=item.icon />
+        </span>
+        <!-- <i v-if="item.icon" class={{item.icon}}></i>-->
+        <span>{{ item.title }}</span>
+    </el-menu-item> 
 </template>
 
 <script>
